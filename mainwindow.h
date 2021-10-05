f1446516588ee4fd9c4c8fd672fc2df1ee91b603
Agrima Bansal <86730647+Agrima0304@users.noreply.github.com>
2021-10-05 14:33:14 +0530

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digit_pressed();
+    void on_decimal_released();
+    void unary_operation_pressed();
+    void on_clear_released();
+    void on_equalto_released();
+    void binary_operation_pressed();
+};
+#endif // MAINWINDOW_H
